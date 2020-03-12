56c8c09580b62ccda9f48c333611b808adc6f9c6
Freefireman <beluh.c.s@gmail.com>
2020-03-12 21:52:02 +0200

    Edit Card

--- src/components/addcard/Addcard.js
+++ src/components/addcard/Addcard.js
@@ -1,36 +1,55 @@
 import React from 'react';
 
 class Addcard extends React.Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
         // const {onAddBtnClickHandler, data} = props;
 
         this.state = {
-            id: "",
-            name: "",
-            username: "",
-            phone: "",
-            address: {city: ""}
+            // id: "",
+            // name: "",
+            // username: "",
+            // phone: "",
+            // address: {city: ""}
+            id: this.props.editUser.id || "",
+            name: this.props.editUser.name ,
+            username: this.props.editUser.username || "",
+            phone: this.props.editUser.phone || "",
+            address: {city: this.props.editUser.address.city || ""}
+        };
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.props !== prevProps) {
+            this.setState({
+                id: this.props.editUser.id || "",
+                name: this.props.editUser.name ,
+                username: this.props.editUser.username || "",
+                phone: this.props.editUser.phone || "",
+                address: {city: this.props.editUser.address.city || ""}
+            })
         }
     }
 
     handlerNameChng = ({target: {value}}) => {
-        this.setState({ name: value });
+        this.setState({name: value});
     };
 
     handlerUsernameChng = ({target: {value}}) => {
-        this.setState({ username: value });
+        this.setState({username: value});
     };
 
     handlerPhoneChng = ({target: {value}}) => {
-        this.setState({ phone: value });
+        this.setState({phone: value});
     };
 
     handlerCityChng = ({target: {value}}) => {
         // debugger
         const {address} = this.state;
         address.city = value;
-        this.setState(() => { return {address} });
+        this.setState(() => {
+            return {address}
+        });
         // console.log("address");
         // console.log(address);
     };
@@ -39,29 +58,60 @@ class Addcard extends React.Component {
         e.preventDefault();
         const {id, name, username, phone, address} = this.state;
         this.props.onAddBtnClickHandler({id, name, username, phone, address});
-        this.setState(() => { return {id: '', name: '', username: '', phone: '', address: {city: ''}}});
-       console.log(this.state);
+        this.setState(() => {
+            return {id: '', name: '', username: '', phone: '', address: {city: ''}}
+        });
+        console.log(this.state);
     };
 
-    render () {
+    onSaveBtnClick = (e) => {
+        e.preventDefault();
+        console.log('onSaveBtnClick');
+        console.log(this.state);
+        const {id, name, username, phone, address} = this.state;
+        this.props.handleSaveEditedCard({id, name, username, phone, address});
+    };
+
+    render() {
+        // console.log("this.props.editUser.phone ", this.props.editUser.phone);
+        console.log(this.state);
         // const {name, username, phone, address:{city}} = this.state;
+        const {name, username, phone, address: {city}} = this.state;
         return (
-            <div className="card" style={{width: '18rem', float: 'left', height: '20rem', padding: '5px'}}>
-                <form>
-                    <input type="text" placeholder="name" className="card-title"
-                    onChange={this.handlerNameChng}/>
-                    <br/>
-                    <input type="text" placeholder="username" className="card-title"
-                    onChange={this.handlerUsernameChng}/>
-                    <br/>
-                    <input type="text" placeholder="phone" className="card-title"
-                    onChange={this.handlerPhoneChng}/>
-                    <br/>
-                    <input type="text" placeholder="city" className="card-title"
-                    onChange={this.handlerCityChng}/>
-                    <br/>
-                    <button className="btn btn-primary" onClick={this.onAddBtnClick}>Add Card</button>
-                </form>
+            <div className="card">
+                {/*{this.props.editUser.phone ? <form>*/}
+                {/*        <input type="text" placeholder="name" className="card-title"*/}
+                {/*               onChange={this.handlerNameChng} value={this.props.editUser.name}/>*/}
+                {/*        <br/>*/}
+                {/*        <input type="text" placeholder="username" className="card-title"*/}
+                {/*               onChange={this.handlerUsernameChng} value={this.props.editUser.username}/>*/}
+                {/*        <br/>*/}
+                {/*        <input type="text" placeholder="phone" className="card-title"*/}
+                {/*               onChange={this.handlerPhoneChng} value={this.props.editUser.phone}/>*/}
+                {/*        <br/>*/}
+                {/*        <input type="text" placeholder="city" className="card-title"*/}
+                {/*               onChange={this.handlerCityChng} value={this.props.editUser.address.city}/>*/}
+                {/*        <br/>*/}
+                {/*        <button className="btn btn-primary" onClick={this.onAddBtnClick}>Add Card</button>*/}
+                {/*        <button className="btn btn-primary">Save Changes</button>*/}
+                {/*    </form> :*/}
+                    <div>
+                        <input type="text" placeholder="name" className="card-title"
+                               onChange={this.handlerNameChng} value={name || ""}/>
+                        <br/>
+                        <input type="text" placeholder="username" className="card-title"
+                               onChange={this.handlerUsernameChng} value={username}/>
+                        <br/>
+                        <input type="text" placeholder="phone" className="card-title"
+                               onChange={this.handlerPhoneChng} value={phone}/>
+                        <br/>
+                        <input type="text" placeholder="city" className="card-title"
+                               onChange={this.handlerCityChng} value={city}/>
+                        <br/>
+                        <button className="btn btn-primary" onClick={this.onAddBtnClick}>Add Card</button>
+                        <button className="btn btn-primary" onClick={this.onSaveBtnClick}>Save Changes</button>
+                    </div>
+                {/*}*/}
             </div>
 
         )
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,7 +1,14 @@
 import React, {useState} from 'react';
 
-function Card({user, countHandler, handleDeleteCard}) {
+function Card({user, countHandler, handleDeleteCard, handleEditCard, editUser}) {
     const [isHidden, setIsHidden] = useState(false);
+
+    if (editUser.id === user.id) {
+        user.name = editUser.name;
+        user.username = editUser.username;
+        user.phone = editUser.phone;
+        user.address.city = editUser.address.city;
+    }
 
     const onHideButtonClick = () => {
         setIsHidden(!isHidden);
@@ -18,17 +25,22 @@ function Card({user, countHandler, handleDeleteCard}) {
         handleDeleteCard(val);
     };
 
+    const onEditButtonClick = (val) => {
+        handleEditCard(val);
+    };
+
     return (
-        <div key={user.id} className="card" style={{width: '18rem', float: 'left', height: '20rem'}}>
-            { !isHidden ? <div style={{width: '18rem', float: 'left', height: '17rem'}}>
+        <div key={user.id} className="card">
+            { !isHidden ? <div className="card-top">
                 <h3 className="card-body">{user.name}</h3>
                 <h4 className="card-title">{user.username}</h4>
                 <h5 className="card-title">{user.phone}</h5>
                 <h5 className="card-title">{user.address.city}</h5>
-            </div> : <div style={{width: '18rem', float: 'left', height: '17rem'}}></div>}
+            </div> : <div className="card-top"></div>}
             <div>
             <button className="btn btn-primary" onClick={ onHideButtonClick } >{isHidden ? "Show" : "Hide"}</button>
             <button className="btn btn-primary" onClick={ onAddButtonClick } >{!isAdded ? "Add" : "Remove"}</button>
+            <button className="btn btn-primary"  onClick={ onEditButtonClick.bind(null, user) }> Edit Card </button>
             <button className="btn btn-primary" onClick={ onDeleteButtonClick.bind(null, user.id) } > Delete Card </button>
             </div>
         </div>
